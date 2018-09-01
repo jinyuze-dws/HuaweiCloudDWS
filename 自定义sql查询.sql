70c85f594661a16b7da55eaf8e71a316874a3895
jinyuze-dws <42887799+jinyuze-dws@users.noreply.github.com>
2018-09-01 16:57:05 +0800

    Delete 自定义sql查询.sql

--- 自定义sql查询.sql
+++
@@ -1,3 +0,0 @@
-
-##在lb_task_meta表中查询出所有和'疑为浸润癌'的所有内容
-select count(*) from dw_kingmed.LB_TASK_META where taskid in (select taskid from dw_kingmed.LB_TASK_DETAIL where item_name='testItemName' and itme_value = '疑为浸润癌')
